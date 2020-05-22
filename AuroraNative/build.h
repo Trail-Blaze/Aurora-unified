bb8a1830a9b183ba38ff3ebc3d4c7ea91317e7bd
Natalie C <cyuubiapps@gmail.com>
2020-05-22 19:03:50 -0400

    Update build.h

--- AuroraNative/build.h
+++ AuroraNative/build.h
@@ -3,10 +3,10 @@
 #define NATIVE_AUTHOR "QXVyb3JhTGF1bmNoZXIvTmF0aXZlIGJ5IEN5dXViaSAoaHR0cHM6Ly90d2l0dGVyLmNvbS91Z3V1TmF0YWxpZSk="
 
 #define DISABLE_PINNING // Disables SSL pinning
-#define LOCALHOST // Enable Localhost servers
+//#define LOCALHOST // Enable Localhost servers
 
-//#define FDEV // Enable Fortnite.Dev servers
+#define FDEV // Enable Fortnite.Dev servers
 #define FDEV_CREDITS "Rm9ydG5pdGUuRGV2IENyZWRpdHM6IFNsdXNoaWEgKGh0dHBzOi8vdHdpdHRlci5jb20vU2x1c2hpYSksIEN5dXViaSAoaHR0cHM6Ly90d2l0dGVyLmNvbS91Z3V1TmF0YWxpZSkgYW5kIEtlbW8gKGh0dHBzOi8vdHdpdHRlci5jb20veGtlbTB4KQ=="
 #define FDEV_DISCORD "Rm9ydG5pdGUuRGV2IERpc2NvcmQ6IGh0dHBzOi8vZGlzY29yZC5nZy9aS2pyVTNQ"
 
-#define VERBOSE // Verbose
+//#define VERBOSE // Verbose
