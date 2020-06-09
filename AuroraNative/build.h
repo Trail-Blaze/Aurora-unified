5f6b46556e3b2c8ba268ed5b0c40ada8cbd04889
Natalie C <cyuubiapps@gmail.com>
2020-06-09 11:48:28 -0400

    v1.1.1.0

--- AuroraNative/build.h
+++ AuroraNative/build.h
@@ -1,10 +1,9 @@
 #pragma once
 
-#define VERSION "v1.1.0.0"
-
 //#define LOCALHOST // Enables Localhost servers
 #define ONLINE // Enables Online servers
 
 #define DISABLE_PINNING // Disables SSL pinning
 #define DISABLE_PROXY // Disables proxy
+
 //#define VERBOSE // Enables verbose logging
